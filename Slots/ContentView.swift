f5a186b27affdf2e4154c405eb8b0341b5abcd65
Peter Sekesan <72456128+Pete-Sekesan@users.noreply.github.com>
2022-07-21 15:56:42 -0400

    Initial layout with assets completed

--- Slots/ContentView.swift
+++ Slots/ContentView.swift
@@ -9,8 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+                Text("SwiftUI Slots!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.top, 20.0)
+            Spacer()
+            Text("placeholder score text")
+                .font(/*@START_MENU_TOKEN@*/.subheadline/*@END_MENU_TOKEN@*/)
+            Spacer()
+            HStack{
+                Image("apple")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 120, height: 120)
+                Image("star")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 120, height: 120)
+                Image("cherry")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 120, height: 120)
+            }
+            Spacer()
+            Button {
+            } label: {
+                Text("Spin")
+                
+                    }
             .padding()
+            .frame(width:120,height: 40)
+            .background(Color.red)
+            .foregroundColor(Color.white)
+            .cornerRadius(90)
+            Spacer()
+
+                
+        }
     }
 }
 
