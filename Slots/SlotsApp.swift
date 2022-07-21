2fb8de0d1ca87952d4b1453e6e9184068439d75f
Peter Sekesan <72456128+Pete-Sekesan@users.noreply.github.com>
2022-07-21 15:31:22 -0400

    Initial Commit

---
+++ Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Peter Sekesan on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
